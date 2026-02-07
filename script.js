d6b99ca13cc4449c2da314c53ddd657d62a99913
GameHub <ellobosolitarioytt@gmail.com>
2026-02-06 22:34:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function seleccionarJuego(nombre) {
+  document.getElementById("juego-seleccionado").textContent = nombre;
+
+  document.getElementById("mega").href = "#";
+  document.getElementById("mediafire").href = "#";
+}
+
