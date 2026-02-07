67ed534577bb00d610b06536122eb1e68ab7315f
GameHub <ellobosolitarioytt@gmail.com>
2026-02-06 22:40:56 -0500

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const lista = document.getElementById("lista");
+const titulo = document.getElementById("titulo");
+
 function seleccionar(nombre) {
   document.getElementById("juego").textContent = nombre;
 }
 
+function mostrarJuegos() {
+  titulo.textContent = "☰ Juegos optimizados";
+  lista.innerHTML = `
+    <li onclick="seleccionar('Kenshi')">Kenshi <span class="tag">64 BITS</span></li>
+    <li onclick="seleccionar('Left 4 Dead')">Left 4 Dead <span class="tag">LAN</span></li>
+    <li onclick="seleccionar('Left 4 Dead 2')">Left 4 Dead 2 <span class="tag">LAN</span></li>
+    <li onclick="seleccionar('Lethal Company')">Lethal Company</li>
+    <li onclick="seleccionar('Little Nightmares')">Little Nightmares</li>
+  `;
+}
+
+function mostrarProgramas() {
+  titulo.textContent = "☰ Programas";
+  lista.innerHTML = `
+    <li onclick="seleccionar('WinRAR')">WinRAR</li>
+    <li onclick="seleccionar('7-Zip')">7-Zip</li>
+    <li onclick="seleccionar('OBS Studio')">OBS Studio</li>
+    <li onclick="seleccionar('Photoshop')">Photoshop</li>
+    <li onclick="seleccionar('VLC Media Player')">VLC Media Player</li>
+  `;
+}
 
