1a510717ac767f3e8927a3dfe81b1538c25f76ed
GameHub <ellobosolitarioytt@gmail.com>
2026-02-06 22:36:56 -0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function seleccionarJuego(nombre) {
-  document.getElementById("juego-seleccionado").textContent = nombre;
-
-  document.getElementById("mega").href = "#";
-  document.getElementById("mediafire").href = "#";
+function seleccionar(nombre) {
+  document.getElementById("juego").textContent = nombre;
 }
+
 
